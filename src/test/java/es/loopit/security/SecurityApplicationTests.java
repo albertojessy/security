1a5641bebb410cae8ce5c34e455876b8b5a41f45
Alberto <arodriguez@loopit.es>
2022-06-04 13:41:11 +0200

    Initial commit

---
+++ src/test/java/es/loopit/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package es.loopit.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
